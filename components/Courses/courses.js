4169318395fe5f2e5efd7e0fc419e31ebac296d9
Hadi14 <asusn43@yahoo.com>
2022-12-08 21:48:45 +0330

    ok1

--- components/Courses/courses.js
+++ components/Courses/courses.js
@@ -3,13 +3,13 @@ temp.innerHTML = `
 <link rel="stylesheet" href="./components/Courses/courses.css">
 <div class="container">
         <div class="imgdiv">
-            <img src="./img/1.jpg" alt="">
+            <img  alt="">
         </div>
         <div class="cont">
-            <h3>Js Course</h3>
-            <p>this ia a Full Course of javaScript</p>
-            <button>Register</button>
-            <button>Show details...</button>
+            <h3></h3>
+            <p class="pdet></p>
+            <button class="regbtn">Register</button>
+            <button class="detbtn">Show details...</button>
         </div>
     </div>
 
@@ -19,24 +19,36 @@ class Course extends HTMLElement {
         super()
         this.attachShadow({ mode: 'open' })
         this.shadowRoot.appendChild(temp.content.cloneNode(true))
+
     }
     connectedCallback() {
-        console.log(this);
-        const h1 = this.shadowRoot.querySelector('h3');
-        h1.innerHTML = this.getAttribute('userjob');
-        h1.addEventListener('click', () => {
-            this.userRemove(this)
-        });
+        const img = this.shadowRoot.querySelector('.imgdiv img');
+        img.src = this.getAttribute('imgsrc');
+
+        const det = this.shadowRoot.querySelector('.pdet');
+        det.innerHTML = "This is a Full Course of " + this.getAttribute('title')
+
+        const tit = this.shadowRoot.querySelector('.cont h3');
+        tit.innerHTML = this.getAttribute('title')
+
+
+        const regbtn = this.shadowRoot.querySelector('.regbtn');
+        regbtn.addEventListener('click', () => {
+            alert("Regestered .")
+        })
+
+        const detbtn = this.shadowRoot.querySelector('.detbtn');
+        detbtn.addEventListener('click', () => {
+            det.classList.toggle("pdet");
+            // console.log("Display");
+        })
+
 
     }
 
-    userRemove(el) {
-        el.remove()
-    }
 
-    disconnectedCallback() {
-        console.log("Removed");
-    }
+
+
     // static observedAttributes() {
     //     return ['userjob']
     // }
